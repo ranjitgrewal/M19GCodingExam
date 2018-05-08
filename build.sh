f2625294bd903d6d68ffcec344ea6b082babbed6
Ranjit Grewal <ranjitr@hotmail.com>
2018-05-08 20:13:14 +0530

    Update build.sh
    added the commands to run the various steps

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-em++ -s WASM=1
+em++ -s WASM=1 --bind -o mulitply.js multiply.cpp
+em++ -s WASM= 1 --bind -o multiply.js multiply.cpp --js-library multiply_in.js
+em++ -s WASM=1 --bind -o multiply.js multiply.cpp --js-library multiply_in.js
